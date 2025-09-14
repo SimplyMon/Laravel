724f3cc8b80a084def4626722f8023585ced13d6
mon <mon.dev005@gmail.com>
2025-09-15 00:18:40 +0800

    laravel practice.

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Routing\RouteAction;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/order', function () {
+    return view('order');
+});
+
+
+
+Route::prefix('/details')->group(function () {
+    Route::get('/students', function () {
+        return 'This is a Students page';
+    });
+    Route::get('/teachers', function () {
+        return 'This is a Teachers page';
+    });
+});
