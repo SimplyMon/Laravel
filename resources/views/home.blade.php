33121d42af407bf631b93fc62e5b6661031523b3
mon <mon.dev005@gmail.com>
2025-09-22 00:18:38 +0800

    learn about model view controller.

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,13 +2,11 @@
     <div class="flex flex-col gap-6 p-6">
         @foreach ($postings as $index => $posting)
             <div class="card bg-white shadow-lg rounded-lg overflow-hidden hover:shadow-xl transition">
-                <!-- Image -->
                 <figure class="h-40">
                     <img src="https://picsum.photos/seed/{{ $index }}/800/300" alt="Post image"
                         class="object-cover w-full h-full" />
                 </figure>
 
-                <!-- Content -->
                 <div class="card-body">
                     <h2 class="card-title text-lg font-semibold text-gray-800">
                         {{ $posting['author'] }}
